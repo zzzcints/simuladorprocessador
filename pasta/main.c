039352fcdaab7edb79effcff810319cb3b3cf6d5
Kris <crisbento@alu.ufc.br>
2026-01-10 16:20:41 -0300

    trabalho finalizado

--- pasta/main.c
+++ pasta/main.c
@@ -7,7 +7,7 @@
 #define MEM_SIZE 65536 // 2^16
 #define MEM_LIMIT 0X2000 // Limite de memória de dados - 8kb palavras
 #define IO_BASE 0xF000 //Endereço de e/s
-#define STACK_BASE 0x2000; //pilha - cresce para baixo 
+#define STACK_BASE 0x2000 //pilha - cresce para baixo 
 
 #define OP_JMP    0    // 0000
 #define OP_JCOND  1    // 0001
@@ -26,10 +26,10 @@
 #define OP_PUSH   14   // 1110
 #define OP_POP    15   // 1111
 
-#define COND_JEQ 0
-#define COND_JNE 1
-#define COND_JLT 2
-#define COND_JGE 3
+#define COND_JEQ 0 // Z= 1
+#define COND_JNE 1 // Z= 0
+#define COND_JLT 2 // Z= 0
+#define COND_JGE 3 // C= 0
 
 
 uint16_t R[16];
@@ -40,14 +40,23 @@ uint16_t PC;
 uint16_t SP;
 
 
+void load_program(const char *filename);
+void print_state();
+int16_t sign_extend(uint16_t val, int bits);
+void update_flags(uint32_t res, bool is_sub);
+
 void reset();
 uint16_t fetch();
 bool execute(uint16_t instr);
 
+int main(int argc, char *argv[]) {
+    if(argc < 2){
+        printf("%s", argv[0]);
+        return 1;
+    }
 
-
-int main() {
     reset();
+    load_program(argv[1]);
 
     MEM[0] = 0xFFFF;
 
@@ -59,13 +68,18 @@ int main() {
         R[15] = PC;
         R[14] = SP;
 
+        if(instr == 0xFFFF){
+            printf_stage();
+            break;
+        }
+
         if (!execute(instr))
             break;
-    }
 
-    //copia os registos de volta para as variaveis, se a instrução mandou pular para outro lugar, então atualizamos o PC
-    PC = R[15];
-    SP = R[14];
+        PC = R[15];
+        SP = R[14];
+
+    }    
 
     printf("llll\n");
     return 0;
@@ -96,10 +110,187 @@ uint16_t fetch() {
     return instr;
 }
 
+
+int16_t sign_extend(uint16_t val, int bits){
+    int shift = 16 - bits;
+    int16_t s_val = (int16_t)(val << shift);
+    return s_val >> shift;
+}
+
+
+void update_flags(uint32_t res, bool is_sub){
+    Z = ((res&0xFFFF) == 0);
+    if(is_sub){
+        C=(res>0xFFFF); // ajusta se o teste exigir lógica específica de ARM/X86  
+    } else {
+        C=(res>0xFFFF);
+    }
+}
+
+
+
 bool execute(uint16_t instr) {
     int opcode = instr >> 12;
+    // Formato 1 (ALU): Opcode(4) | Rd(4) | Rm(4) | Rn(4)
+    uint8_t rd = (instr >> 12) & 0xF;
+    uint8_t r_dest = (instr >> 8) & 0xF;
+    uint8_t r_src1 = (instr >> 4) & 0xF;
+    uint8_t r_src2 = instr & 0xF;
+
+    //imediados
+    uint8_t imm8 = instr & 0xFF;
+    uint16_t imm12 = instr & 0xFFF;
+    uint16_t imm10 = instr & 0x3FF;
+
+    //auxiliares
+    uint32_t res = 0;
+    int16_t offset = 0;
+    uint16_t addr = 0;
+
+    switch (opcode){
+            case OP_JMP:
+                offset = sing_extend(imm12, 12);
+                R[15] += offset;
+                break;
+
+            case OP_JCOND:
+            {
+                    uint8_t cond = (instr >> 10) & 0x3;
+                    offset = sing_extend(instr & 0x3FF,10);
+                    bool jump = false; 
+                    switch(cond){
+                        case COND_JEQ: jump = Z; break;
+                        case COND_JNE: jump = !Z; break;
+                        case COND_JLT: jump = (!Z && C); break;
+                        case COND_JGE: jump = (!C && Z); break;
+                    }
+                    if(jump) R[15] += offset;
+                    break;
+            }
+
+            case OP_LDR:
+                    addr = R[r_src1] + r_src2;
+                    if(addr == 0xF002){
+                        int val;
+                        scanf("%d", &val);
+                        R[r_dest] = val;
+                    } else if(addr == 0xF000){
+                        R[r_dest] = getchar();
+
+                    } else if (addr < MEM_SIZE){
+                        R[r_dest] = MEM[addr];
+                        accessed[addr] = true;
+                    }
+                    break;
+
+             case OP_STR:
+                    addr = R[r_src1] + r_src2;
+                    if(addr == 0xF003){
+                        printf("%d\n", (short)R[r_dest]);
+                    } else if (addr == 0xF001){
+                        printf("%c", (char)R[r_dest]);
+                    } else if(addr < MEM_SIZE){
+                        MEM[addr] = R[r_dest];
+                        accessed[addr] = true;
+                    }   
+                    break;
+            case OP_MOV:
+                    R[r_dest]= sign_extend(imm8, 8);
+                    break;
+            case OP_ADD:
+                    res = (uint32_t)R[r_src1] + R[r_src2];
+                    R[r_dest] = (uint16_t)res;
+                    update_falgs(res, false);
+                    break;
+            case OP_ADDI:
+                    res = (uint32_t)R[r_src1] + r_src2;
+                    R[r_dest] = (uint16_t)res;
+                    update_flags(res, false);
+                    break;
+            case OP_SUB:
+                    res = (uint32_t) R[r_src1] - R[r_src2];
+                    R[r_dest] = (uint16_t)res;
+                    update_flags(res, true);
+                    break;
+            case OP_SUBI:
+                    res = (uint32_t)R[r_src1] - r_src2;
+                    R[r_dest] = (uint16_t)res;
+                    update_flags(res, true);
+                    break;
+            case OP_AND:
+                    R[r_dest] = R[r_src1] & R[r_src2];
+                    Z = (R[r_dest] == 0);
+                    C = false;
+                    break;
+            case OP_OR:
+                    R[r_dest] = R[r_src1] | R[r_src2];
+                    Z = (R[r_dest] == 0);
+                    C = false;
+                    break;
+            case OP_SHR:
+                    R[r_dest] = R[r_src1] >> r_src2; 
+                    Z = (R[r_dest] == 0);
+                    C = false;
+                    break;
+            case OP_SHL:
+                    res = (uint32_t)R[r_src1] << r_src2;
+                    R[r_dest] = (uint16_t)res;
+                    Z = (R[r_dest] == 0);
+                    C = (res > 0xFFFF);
+                    break;
+            case OP_CMP: 
+                    res = (uint32_t)R[r_dest] - R[r_src1]; 
+                    update_flags(res, true);
+                    break;
+            case OP_PUSH: 
+                    R[14]--; 
+                    if (R[14] < MEM_SIZE) MEM[R[14]] = R[r_dest]; 
+                    break;
+            case OP_POP: 
+                    if (R[14] < MEM_SIZE) R[r_dest] = MEM[R[14]];
+                    R[14]++;
+                    break;
+            default:
+            return false;
+    }
+
     if (instr == 0xFFFF) {
         return false;
     }
     return true;
+}
+
+void load_program(const char *filename) {
+    FILE *fp = fopen(filename, "r");
+    if (!fp) {
+        printf("Erro ao abrir arquivo %s\n", filename);
+        exit(1);
+    }
+
+
+
+    unsigned int addr, val;
+    while (fscanf(fp, "%x %x", &addr, &val) == 2) {
+        if (addr < MEM_SIZE) {
+            MEM[addr] = (uint16_t)val;
+        }
+    }
+    fclose(fp);
+}
+
+void print_state() {
+    for (int i = 0; i < 16; i++) printf("R%d=0x%04hX\n", i, R[i]); //registrador
+    // flags
+    printf("Z=%d\n", Z);
+    printf("C=%d\n", C);
+    // memória de dados
+    for (int i = 0; i < MEM_LIMIT; i++) {
+        if (accessed[i]) printf("[%04X]=0x%04hX\n", i, MEM[i]);
+    }
+    // pilha
+    if (SP != STACK_BASE){
+        for (int i = STACK_BASE - 1; i >= SP; i--){
+             printf("[%04X]=0x%04hX\n", i, MEM[i]);
+        }
+    }
 }
