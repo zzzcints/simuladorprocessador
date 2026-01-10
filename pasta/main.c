10d507236aed71737ee6bf928eae153bbb73143f
Kris <crisbento@alu.ufc.br>
2026-01-10 14:57:48 -0300

    adicionado as informações enviadas via whatsapp

--- pasta/main.c
+++ pasta/main.c
@@ -1,14 +1,44 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MEM_SIZE 65536 // 2^16
+#define MEM_LIMIT 0X2000 // Limite de memória de dados - 8kb palavras
+#define IO_BASE 0xF000 //Endereço de e/s
+#define STACK_BASE 0x2000; //pilha - cresce para baixo 
+
+#define OP_JMP    0    // 0000
+#define OP_JCOND  1    // 0001
+#define OP_LDR    2    // 0010
+#define OP_STR    3    // 0011
+#define OP_MOV    4    // 0100
+#define OP_ADD    5    // 0101
+#define OP_ADDI   6    // 0110
+#define OP_SUB    7    // 0111
+#define OP_SUBI   8    // 1000
+#define OP_AND    9    // 1001
+#define OP_OR     10   // 1010
+#define OP_SHR    11   // 1011
+#define OP_SHL    12   // 1100
+#define OP_CMP    13   // 1101
+#define OP_PUSH   14   // 1110
+#define OP_POP    15   // 1111
+
+#define COND_JEQ 0
+#define COND_JNE 1
+#define COND_JLT 2
+#define COND_JGE 3
+
 
 uint16_t R[16];
 uint16_t MEM[MEM_SIZE];
+bool accessed[MEM_SIZE];
+bool Z, C;
 uint16_t PC;
 uint16_t SP;
-bool Z, N;
+
 
 void reset();
 uint16_t fetch();
@@ -23,9 +53,19 @@ int main() {
 
     while (1) {
         uint16_t instr = fetch();
+
+        //copiar as variaveis para os registos para se precisar de ler o PC ou SP
+        //os valores estão certos em R[15]; 
+        R[15] = PC;
+        R[14] = SP;
+
         if (!execute(instr))
             break;
     }
+
+    //copia os registos de volta para as variaveis, se a instrução mandou pular para outro lugar, então atualizamos o PC
+    PC = R[15];
+    SP = R[14];
 
     printf("llll\n");
     return 0;
@@ -57,6 +97,7 @@ uint16_t fetch() {
 }
 
 bool execute(uint16_t instr) {
+    int opcode = instr >> 12;
     if (instr == 0xFFFF) {
         return false;
     }
